aafc9077f2aaa922a3d9adfe5e9a4572bb2d058d
CryptoSnowPrince <topdirector2017@gmail.com>
2022-04-19 23:20:59 +0200

    add mumbai testnet

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const bsc_testnet = require ('./networks/bsc_testnet')
 const rinkeby = require ('./networks/rinkeby')
 const bsc = require ('./networks/bsc_mainnet')
 const matic = require ('./networks/matic_mainnet')
+const mumbai = require ('./networks/matic_testnet')
 
 const chain_id = {
     '4' : rinkeby,
-    '97' : bsc_testnet,
     '56' : bsc,
     '137' : matic,
+    '97' : bsc_testnet,
+    '80001': mumbai,
 }
 
 bsc.bridge.on('SwapInitialized', async (from,to,ammount,ticker,chainTo,chainFrom,nonce) =>{
@@ -89,6 +91,34 @@ bsc_testnet.bridge.on('SwapInitialized', async (from,to,ammount,ticker,chainTo,c
     );
 
     const signature = await bsc_testnet.wallet.signMessage(ethers.utils.arrayify(message));
+
+    const tx = await chain_id[chainTo].bridge.redeem(from, to, ammount, ticker, chainFrom, chainTo, nonce, signature)
+    const txhash = await tx.wait()
+
+    console.log(txhash.transactionHash)
+
+});
+
+mumbai.bridge.on('SwapInitialized', async (from,to,ammount,ticker,chainTo,chainFrom,nonce) =>{
+
+    console.log(`
+        MUMBAI TESTNET Event Swap Emitted :
+        =================================================
+        from: ${from}
+        to: ${to}
+        ammount: ${ammount}
+        ticker: ${ticker}
+        chainTo: ${chainTo}
+        chainFrom: ${chainFrom}
+        nonce: ${nonce}
+    `);
+
+    const message = ethers.utils.solidityKeccak256(
+        ["address", "address", "uint256", "string", "uint256", "uint256", "uint256"],
+        [from, to, ammount, ticker, chainFrom, chainTo, nonce]
+    );
+
+    const signature = await mumbai.wallet.signMessage(ethers.utils.arrayify(message));
 
     const tx = await chain_id[chainTo].bridge.redeem(from, to, ammount, ticker, chainFrom, chainTo, nonce, signature)
     const txhash = await tx.wait()
