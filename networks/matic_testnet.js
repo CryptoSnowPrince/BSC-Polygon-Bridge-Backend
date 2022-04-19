24e85d7d5eaf6d0c006d87084516e9a45da3c628
CryptoSnowPrince <topdirector2017@gmail.com>
2022-04-19 23:16:10 +0200

    update Bridge.json add matic_testnet.js

---
+++ networks/matic_testnet.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const ethers = require('ethers');
+
+const VALIDATOR_KEY = process.env.VALIDATOR_KEY
+const WSS_URL = process.env.MUMBAI_WS
+const BRIDGE_ADDRESS = process.env.MUMBAI_ADDRESS
+const provider = new ethers.providers.WebSocketProvider(WSS_URL);
+const wallet = new ethers.Wallet(VALIDATOR_KEY);
+const account = wallet.connect(provider);
+const bridgeABI = require('../abi/Bridge.json')
+
+const bridge = new ethers.Contract(BRIDGE_ADDRESS, bridgeABI, account);
+
+module.exports = { bridge, provider, wallet }
